8ef29bc9fcd237f33400f33fbbb92c3f4c5907d1
Tomek Karwowski <to.karwowski@gmail.com>
2021-06-10 23:13:36 +0200

    add git clone script

---
+++ gitclone.sh
@@ -0,0 +1,3 @@
+git clone https://github.com/io2-2021-wed-brodka/G_Backend.git
+git clone https://github.com/io2-2021-wed-brodka/G_Admin.git
+git clone https://github.com/io2-2021-wed-brodka/G_User-Tech.git
